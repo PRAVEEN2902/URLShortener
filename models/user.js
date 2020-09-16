d19690e1554f6dd19e28ae270e15c4240e7602a4
Praveen Kumar <praveenkumarpdkt25@gmail.com>
2020-09-16 18:36:57 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -15,14 +15,13 @@ const url=new Schema1({
          const current=new Date()
          return current.setDate(current.getDate()+2)
      }},
-   // expireAt:{type:Date,default:new Date('Sep 12,2020 23:59:00')},
+   
     full:String,
     short:String,
     email:String,
    
 })
 url.index({"expireAt": 1 },{ expireAfterSeconds: 0 });
-//url.plugin(ttl, { ttl: 5000, reap: false });
 const model=mongoose.model('users',val)
 const shURL=mongoose.model('url',url)
 module.exports={model,shURL}
